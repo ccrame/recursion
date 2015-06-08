aa034e289f28a0f66ae3e8e8660f22703afe2241
Charles Crame <ccrame@gmail.com>
2015-06-08 14:37:51 -0700

    Added a recursion function to getElementsByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  var result = [];
+  var recursion = function(item){
+    if(Array.isArray(item)){
+
+    } else if ( typeof item === 'object') {
+
+    } else {
+
+    } //end of if chain
+
+  }; //end of recursion
+
+  recursion();
+  return result;
 };
