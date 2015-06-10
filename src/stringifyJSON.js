66eab21f4f4cdc91e12aacc3468d8ebb1d1c5764
Charles Crame <ccrame@gmail.com>
2015-06-10 01:12:06 -0700

    Completed stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,18 +4,37 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  var result = "";
+  
+  var recursion = function(input){
+    if(input === null || input === undefined){
+      return String(input);
+    // if input is an array
+    } else if (Array.isArray(input)){
+      var result = [];
+      _.each(input,function(value){
+        result.push(recursion(value));
+      });
+      return '[' + result.join(',') + ']';
 
-  var recursion = function(item){
-    if(Array.isArray(item)){
+    // if input is an object
+    } else if (typeof input === 'object'){
+      var result = [];
+      _.each(input,function(value,key){
+        if(typeof value != 'undefined' && typeof value !== 'function'){
+          result.push('"' + String(key) + '":' + recursion(value)); 
+        }
+      });
+      return '{' + result.join(',') + '}';
 
-    } else if (typeof item === 'object') {
+    // if input is a string
+    } else if ( typeof input === 'string') {
+      return '"' + input + '"';
 
+      //if input is anything else
     } else {
-      result += String(item);
+      return String(input);
     }//end of if/else chain
   }//end of recursion function
 
-  recursion(obj);
-  return result;
+  return recursion(obj);
 };
